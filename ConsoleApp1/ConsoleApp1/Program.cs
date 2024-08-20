375c7a4283478a36164127f80beacae105a95077
dylesi <dyles@hotmail.fi>
2024-08-20 11:26:38 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nimi");
+            string vastaus1 = Console.ReadLine();
+            viimeinenkirjain(vastaus1);
+            Console.ReadLine();
+        }
+        static void positiivinen(int luku1)
+        { 
+            if (luku1 < 0) 
+            {
+                Console.WriteLine(luku1 - (luku1 * 2));
+            }
+            else
+            {
+                Console.WriteLine(luku1);
+            }
+        }
+
+        static void viimeinenkirjain(string input1)
+        {
+            int pituus = input1.Length;
+
+            Console.WriteLine("Viimeinen kirjain on: " + input1[pituus -1]);
+
+        }
+
+    }
+}
